3535a952dae5b20b372e4a7ad1321b1082941b80
Lucas <git@roang.info>
2018-12-05 00:31:32 +0100

    Prepare for 2015 d3 c2

--- aoc2015/d3/d3.py
+++ aoc2015/d3/d3.py
@@ -3,21 +3,28 @@ from collections import defaultdict
 with open("input.txt") as input_file:
     input_data = input_file.readline()
 
-houses = defaultdict(int)
-x = 0
-y = 0
-count = 0
-houses[(x,y)] += 1
-for direction in input_data:
-    count += 1
+houses_1 = defaultdict(int)
+houses_2 = defaultdict(int)
+pos = {
+    'c1': {
+        'x': 0, 'y': 0
+    },
+    'santa': {
+        'x': 0, 'y': 0
+    },
+    'robo_santa': {
+        'x': 0, 'y': 0
+    }
+}
+houses_1[(pos['c1']['x'],pos['c1']['y'])] += 1
+for i, direction in enumerate(input_data):
     if direction == "^":
-        y -= 1
+        pos['c1']['y'] -= 1
     elif direction == ">":
-        x += 1
+        pos['c1']['x'] += 1
     elif direction == "<":
-        x -= 1
+        pos['c1']['x'] -= 1
     elif direction == "v":
-        y += 1
-    houses[(x,y)] += 1
-print(count)
-print(len(houses))
+        pos['c1']['y'] += 1
+    houses_1[(pos['c1']['x'],pos['c1']['y'])] += 1
+print(len(houses_1))
