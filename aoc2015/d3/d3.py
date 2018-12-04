ba6278b7430e2afab059fed51f4492770f616b2c
Lucas <git@roang.info>
2018-12-05 00:36:04 +0100

    Add 2015 d3 c2

--- aoc2015/d3/d3.py
+++ aoc2015/d3/d3.py
@@ -16,15 +16,24 @@ pos = {
         'x': 0, 'y': 0
     }
 }
-houses_1[(pos['c1']['x'],pos['c1']['y'])] += 1
+houses_1[(0,0)] += 1
+houses_2[(0,0)] += 1
 for i, direction in enumerate(input_data):
+    mover = 'robo_santa' if i % 2 else 'santa'
     if direction == "^":
         pos['c1']['y'] -= 1
+        pos[mover]['y'] -= 1
     elif direction == ">":
         pos['c1']['x'] += 1
+        pos[mover]['x'] += 1
     elif direction == "<":
         pos['c1']['x'] -= 1
+        pos[mover]['x'] -= 1
     elif direction == "v":
         pos['c1']['y'] += 1
+        pos[mover]['y'] += 1
     houses_1[(pos['c1']['x'],pos['c1']['y'])] += 1
-print(len(houses_1))
+    houses_2[(pos[mover]['x'],pos[mover]['y'])] += 1
+print(f'Santa delivered to {len(houses_1)} houses')
+print(f'Santa and Robo_santa delivered to {len(houses_2)} houses')
+
