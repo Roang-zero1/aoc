0183eff8e5edee310e7afabf008b313349dc9f9b
Lucas <git@roang.info>
2018-12-05 01:24:58 +0100

    Add solution for 2015 d5

---
+++ aoc2015/d5/d5.py
@@ -0,0 +1,38 @@
+vowels = ['a','e','i','o','u']
+naughty = ['ab', 'cd', 'pq', 'xy']
+
+nice = 0
+with open('input.txt') as input_file:
+    for line in input_file.readlines():
+        contains_naughty = any(x in line for x in naughty)
+        double = False
+        prev_char = ''
+        v_count = 0
+        for char in line:
+            if char in vowels:
+                v_count += 1
+            if prev_char == char:
+                double = True
+            elif double == False:
+                prev_char = char
+            if double and v_count >= 3:
+                break
+        if v_count >= 3 and not contains_naughty and double:
+            nice += 1
+
+print(f'Found {nice} nice strings')
+
+nice = 0
+with open('input.txt') as input_file:
+    for line in input_file.readlines():
+        double = False
+        double_pairs = False
+        for i in range(len(line)-2):
+            if line[i] == line[i+2]:
+                double = True
+            if line.count(f'{line[i]}{line[i+1]}') > 1:
+                double_pairs = True
+        if double and double_pairs:
+            nice += 1
+
+print(f'Found {nice} nice strings')
