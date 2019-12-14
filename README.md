1fe2f0f7aec613b92b1aad6e3f4d539c17ee9dcf
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-12-14 02:54:17 +0100

    Remove tests.py
    Use automatic test discovery instead

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# aoc
+# Advent of code
+
 Advent of Code solutions
 
 My solutions to the [Advent of Code](https://adventofcode.com/) Event Challenges
+
+## Run tests
+
+Use `python -m unittest` to run the python test-cases
--- tests.py
+++
@@ -1,8 +0,0 @@
-#!python
-import unittest
-from aoc2019.tests.d1 import Test2019Day1
-from aoc2019.tests.d3 import Test2019Day3
-from aoc2019.tests.processor import Test2019Processor
-
-if __name__ == "__main__":
-    unittest.main()
