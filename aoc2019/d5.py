0e6a395fb59d8460a00db538a7ad9504fc0050f6
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-12-15 01:34:19 +0100

    Add solution for 2019 D5 part 1

---
+++ aoc2019/d5.py
@@ -0,0 +1,39 @@
+#!python
+
+from aoc.classes import AoCResult, AoCReturn
+from typing import List
+
+from aoc2019.processor import Processor, ResultMode
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def main(input: List[str]):
+    code = list(map(int, input[0].split(",")))
+    processor = Processor(code)
+    result = processor.run()
+    if result[0] != ResultMode.INPUT:
+        raise Exception("Unexpected result, verify input")
+
+    result = processor.continue_operation(1)
+    diagnostic_code = -1
+    while True:
+        if result[0] == ResultMode.OUTPUT:
+            diagnostic_code = result[1]
+            logger.debug("Status of operation %s", result[1])
+        elif result[0] == ResultMode.FINISHED:
+            break
+        result = processor.continue_operation()
+
+    if diagnostic_code == -1:
+        raise Exception("Failed to obtain diagnostic code")
+
+    result_1 = AoCResult(diagnostic_code, "diagnostic code")
+
+    return AoCReturn((result_1, AoCResult(None, "NYI")))
+
+
+if __name__ == "__main__":
+    raise Exception("Please use the run script")
